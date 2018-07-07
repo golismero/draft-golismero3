2e07581317005d1e7309a11459609142c922b3b9
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2018-07-07 18:40:09 +0200

    WIP: plugin_runner input dict instead of json.

--- golismero3/plugin.py
+++ golismero3/plugin.py
@@ -7,12 +7,12 @@ def plugin_runner(cmd):
         if not inp:
             return None
         b = bytearray()
-        b.extend(inp.encode())
+        b.extend(json.dumps(inp).encode())
         return b
     def _parse_json(out):
         return json.loads(out)
     def _runner(inp=None):
-        input_data = [_parse_json(inp)]
+        input_data = [inp]
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, stdin=subprocess.PIPE)
         stdout, stderr = p.communicate(input=_parse_input(inp))
         if stderr:
